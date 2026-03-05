b13a66bfea0ba35a5d147e06f28bd5de1548cfc9
Clea <clea@Cleas-Mac-mini.local>
2026-03-05 10:54:59 -0500

    initial: hello world

---
+++ index.js
@@ -0,0 +1,9 @@
+const http = require('http');
+const port = process.env.PORT || 3000;
+
+http.createServer((req, res) => {
+  res.writeHead(200, { 'Content-Type': 'text/plain' });
+  res.end('Hello from Clea 🎭\n');
+}).listen(port, () => {
+  console.log(`Running on port ${port}`);
+});
